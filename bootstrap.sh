eb65b78ea6070fedfef742acd333d532e165849c
Andrew Lyu <apemost@gmail.com>
2019-01-28 21:13:46 +0800

    Fix bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 
 git pull origin master
 
-rsync -avh --no-perms vimrc
-rsync -avh --no-perms --exclude .DS_Store vim ~/.vim
+rsync -avh --no-perms vimrc ~/.vimrc
+rsync -avh --no-perms --exclude .DS_Store vim/* ~/.vim/
