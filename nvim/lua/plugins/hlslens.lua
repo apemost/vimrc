da2871d678d0fb111a04eb253a7dc1a235f75980
Andrew Lyu <apemost@gmail.com>
2026-07-16 17:15:38 +0800

    feat(nvim): add nvim-hlslens search lens plugin
    Add nvim-hlslens with keymaps for n/N/*/g*/g# that trigger the lens UI,
    and enable calm_down to auto-clear lens on cursor movement.

---
+++ nvim/lua/plugins/hlslens.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "kevinhwang91/nvim-hlslens",
+    event = "CmdlineEnter",
+    keys = {
+      {
+        "n",
+        [[<Cmd>execute('normal! ' .. v:count1 .. 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        desc = "Next search result with lens",
+      },
+      {
+        "N",
+        [[<Cmd>execute('normal! ' .. v:count1 .. 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        desc = "Previous search result with lens",
+      },
+      { "*", [[*<Cmd>lua require('hlslens').start()<CR>]], desc = "Search word forward with lens" },
+      { "#", [[#<Cmd>lua require('hlslens').start()<CR>]], desc = "Search word backward with lens" },
+      { "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], desc = "Search partial word forward with lens" },
+      { "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], desc = "Search partial word backward with lens" },
+    },
+    config = function()
+      require("hlslens").setup({
+        -- Clear lens when cursor moves out of the matched instance range,
+        -- approximating is.vim's auto-nohlsearch behavior.
+        calm_down = true,
+      })
+    end,
+  },
+}
