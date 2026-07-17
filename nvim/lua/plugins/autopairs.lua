7fa30d0222757e4f0db35dccd1c2a4d8d21c1780
Andrew Lyu <apemost@gmail.com>
2026-07-17 15:05:15 +0800

    feat(nvim): add vim-repeat and nvim-autopairs

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,9 @@
+-- windwp/nvim-autopairs: auto-close brackets / quotes in insert mode.
+-- TODO: opt-in deep integration with blink-cmp (CR accept + pairs) if needed.
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
+}
---
+++ nvim/lua/plugins/repeat.lua
@@ -0,0 +1,5 @@
+-- tpope/vim-repeat: lets `.` repeat plugin maps (surround, fugitive, ...).
+-- Pure vimscript, no setup; load eagerly so dot-repeat works from startup.
+return {
+  { "tpope/vim-repeat", lazy = false },
+}
