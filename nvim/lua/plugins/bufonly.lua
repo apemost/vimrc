e51b752e55b0d33d627a30c52d976335e5d44bcc
Andrew Lyu <apemost@gmail.com>
2026-07-20 11:42:24 +0800

    feat(nvim): add vim-bufonly plugin for deleting other buffers

---
+++ nvim/lua/plugins/bufonly.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "schickling/vim-bufonly",
+    keys = {
+      {
+        "<Leader>bo",
+        "<Cmd>BufOnly<CR>",
+        desc = "Delete other buffers",
+      },
+    },
+  },
+}
