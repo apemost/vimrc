b4a1646f02aabca1d6aef90ab050323afe1a2fd4
Andrew Lyu <apemost@gmail.com>
2026-07-24 22:39:47 +0800

    feat(nvim): configure treesitter parsers and enable highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,49 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 --*********************************************************************
 
+-- Parsers for common languages, installed on startup (no-op if already
+-- installed).
+local ensure_installed = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "go",
+  "html",
+  "java",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "query",
+  "regex",
+  "rust",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install(ensure_installed)
+
+      -- Highlighting is not enabled automatically; start it whenever a
+      -- parser is available for the buffer's filetype.
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("TreesitterHighlight", { clear = true }),
+        callback = function(event)
+          pcall(vim.treesitter.start, event.buf)
+        end,
+      })
+    end,
   },
 }
